f157eb0caee1b01b0a4c18cea7bd6751ee261316
14raryana <14rarayna@gmail.com>
2021-05-19 11:28:10 -0400

    Just added a port const so the site can load on heroku and github pages

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
@@ -22,6 +23,6 @@ io.on("connection", (socket) => {
     });
 })
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
     console.log(`Server Listening on port: 3000`);
 });
