937c68b7b7550a2f521baeca6780b20a7c477cc6
14raryana <14rarayna@gmail.com>
2021-05-19 11:17:11 -0400

    Basic Socket.io chat app. Server listens on port 3000 and allows people to join and message each other. Need to add broadcast rather than emit so the sender doesn't get message as well. Right now, everyone, including the sender, gets the message being sent.

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+    console.log("A user has connected");
+
+    socket.on("chat message", (msg) => {
+        console.log(`Message: ${msg}`);
+        io.emit("chat message", msg);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User has disconnected");
+    });
+})
+
+server.listen(3000, () => {
+    console.log(`Server Listening on port: 3000`);
+});
